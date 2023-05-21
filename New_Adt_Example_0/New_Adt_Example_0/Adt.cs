47d268f8e21cc7c18fcd451675d926041bae30ea
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-21 16:49:37 +0330

    Add files via upload

---
+++ New_Adt_Example_0/New_Adt_Example_0/Adt.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Adt_Example_0
+{
+    class Adt
+    {
+        public int[] dq;
+        public int rear1;
+        public int rear2;
+        public int max;
+
+        public Adt(int size)
+        {
+            dq = new int[size];
+            max = size;
+            rear1 = -1;
+            rear2 = max;
+        }
+        public bool isfull()
+        {
+            if ((rear1 + 1) == rear2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool isempty(bool ch)
+        {
+            if (ch)
+            {
+                return (rear1 == -1);
+            }
+            else
+            {
+                return (rear2 == max);
+            }
+        }
+        public void insert(int data)
+        {
+            if (isfull())
+            {
+                Console.WriteLine("New Adt Is Full !");
+                return;
+            }
+            if (data >= 0)
+            {
+                dq[++rear1] = data;
+            }
+            else
+            {
+                dq[--rear2] = data;
+            }
+        }
+        public int remove(bool ch)
+        {
+            if (isempty(ch))
+            {
+                Console.WriteLine("Queue Is Empty !");
+                return -1;
+            }
+            int d;
+            if (ch)
+            {
+                d = dq[0];
+                Console.WriteLine($"{d} Is Deleted...");
+                for (int i = 0; i <= rear1; i++)
+                {
+                    dq[i] = dq[i + 1];
+                }
+                rear1--;
+            }
+            else
+            {
+                d = dq[max - 1];
+                Console.WriteLine($"{d} Is Deleted...");
+                for (int i = max - 1; i >= rear2; i--)
+                {
+                    dq[i] = dq[i - 1];
+                }
+                rear2++;
+            }
+            return d;
+        }
+        public int length()
+        {
+            return ((rear1 + 1) + (max - rear2));
+        }
+        public void print()
+        {
+            for (int i = 0; i < max; i++)
+            {
+                if (dq[i] == 0)
+                {
+                    Console.Write(". ");
+                }
+                else
+                {
+                    Console.Write((dq[i] + " . "));
+                }
+            }
+        }
+    }
+}
---
+++ New_Adt_Example_0/New_Adt_Example_0/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Adt_Example_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adt q = new Adt(20);
+            q.insert(34);
+            q.insert(54);
+            q.insert(-23);
+            q.insert(99);
+            q.insert(59);
+            q.insert(-16);
+            q.insert(-19);
+            q.insert(-7);
+            q.insert(84);
+            q.insert(-90);
+            q.insert(-13);
+            q.insert(77);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Queue Is : ");
+            q.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Enter The Choice ( R Or L )");
+            string ch1 = Console.ReadLine();
+            if (ch1 == "l" || ch1 == "L")
+                q.remove(true);
+            else if (ch1 == "r" || ch1 == "R")
+                q.remove(false);
+            Console.WriteLine("\nNow Queue Is : ");
+            q.print();
+            Console.ResetColor();
+            Console.WriteLine("\n\n");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter The Choice ( R Or L )");
+            string ch2 = Console.ReadLine();
+            if (ch2 == "l" || ch2 == "L")
+                q.remove(true);
+            else if (ch2 == "r" || ch2 == "R")
+                q.remove(false);
+            Console.WriteLine("\nNow Queue Is : ");
+            q.print();
+            Console.ResetColor();
+            Console.WriteLine("\n\n");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("The Length Of New Adt ( Double Queue Is ) : ");
+            int l = q.length();
+            Console.WriteLine(l);
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+        }
+    }
+}
